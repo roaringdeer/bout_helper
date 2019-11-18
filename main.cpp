d8f1176d8717119bebbd3cd5f6b43187d738da2e
Jacek Lichwa <ytos.ventus@gmail.com>
2019-11-19 00:51:52 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by jacek on 2019-11-18.
+//
+
+#include "main.hpp"
+#include <iostream>
+
+
+int main(){
+    std::cout<<"Hello world";
+}
---
+++ main.hpp
@@ -0,0 +1,14 @@
+//
+// Created by jacek on 2019-11-18.
+//
+
+#ifndef BOUT_HELPER_MAIN_HPP
+#define BOUT_HELPER_MAIN_HPP
+
+
+class main {
+
+};
+
+
+#endif //BOUT_HELPER_MAIN_HPP
